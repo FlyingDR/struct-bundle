b3a67712b61c1b4bcae46ea153a11d1a01be2dc6
Alexander Grimalovsky <alexander.grimalovsky@gmail.com>
2013-09-04 15:27:37 +0400

    Updated namespaces map definition for structures components

--- FlyingStructBundle.php
+++ FlyingStructBundle.php
@@ -2,11 +2,10 @@
 
 namespace Flying\Bundle\StructBundle;
 
-use Flying\Struct\Configuration\NamespacesMap;
+use Flying\Struct\Configuration;
 use Flying\Struct\ConfigurationManager;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Flying\Struct\Configuration;
 
 class FlyingStructBundle extends Bundle
 {
@@ -17,30 +16,12 @@ class FlyingStructBundle extends Bundle
     public function boot()
     {
         parent::boot();
-        /** @var $configuration Configuration */
         $configuration = $this->container->get('flying_struct.configuration');
         $nsMap = $this->container->getParameter('flying_struct.nsmap');
-        $this->registerNsMap($configuration->getAnnotationNamespacesMap(), $nsMap['annotation']);
-        $this->registerNsMap($configuration->getPropertyNamespacesMap(), $nsMap['property']);
-        $this->registerNsMap($configuration->getStructNamespacesMap(), $nsMap['struct']);
+        $configuration->getAnnotationNamespacesMap()->add($nsMap['annotation']);
+        $configuration->getPropertyNamespacesMap()->add($nsMap['property']);
+        $configuration->getStructNamespacesMap()->add($nsMap['struct']);
         ConfigurationManager::setConfiguration($configuration);
-    }
-
-    /**
-     * Register given namespaces into given namespaces map
-     *
-     * @param NamespacesMap $nsMap
-     * @param array $namespaces
-     * @return void
-     */
-    protected function registerNsMap(NamespacesMap $nsMap, array $namespaces)
-    {
-        foreach ($namespaces as $alias => $ns) {
-            if (!is_string($alias)) {
-                $alias = strtolower(str_replace('\\', '_', $ns));
-            }
-            $nsMap->add($alias, $ns);
-        }
     }
 
 }
